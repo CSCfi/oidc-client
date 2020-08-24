4904303643dc569388ee50f079cc00453c61b820
teemu.kataja <teemu.kataja@csc.fi>
2020-08-24 11:08:29 +0300

    add github actions

--- tests/coveralls.py
+++ tests/coveralls.py
@@ -8,9 +8,9 @@ import sys
 # Solution provided by https://stackoverflow.com/questions/32757765/conditional-commands-in-tox-tox-travis-ci-and-coveralls
 
 if __name__ == '__main__':
-    if 'TRAVIS' in os.environ:
+    if 'COVERALLS_REPO_TOKEN' in os.environ:
         rc = call('coveralls')
-        sys.stdout.write("Coveralls report from TRAVIS CI.\n")
-        raise SystemExit(rc)
+        sys.stdout.write("Coveralls report\n")
+        # raise SystemExit(rc)
     else:
-        sys.stdout.write("Not on TRAVIS CI.\n")
+        sys.stdout.write("Not on CI.\n")
